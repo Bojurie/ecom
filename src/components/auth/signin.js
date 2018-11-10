41f71807b6f1a2590cf3cbc0e7ece33967a0c12a
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-11-10 12:31:41 -0800

    built sign in and sign up routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    render() {
+        return (
+           <div className='sign-in'>
+                sign inn
+           </div>
+        )
+    }
+}
+export default SignIn;

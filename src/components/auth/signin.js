2e70f35c52d2704a9a32d0174070d115f850c527
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-11-15 15:02:05 -0800

    finished header feature

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 import SignInForm from './signinForm';
 import PageTitle from '../pageTitle';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignIn extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
 
     onSubmit = (fields) => {
         console.log(fields);
@@ -12,11 +20,13 @@ class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
-            <PageTitle className='sign-in__page-title' title='Login'/>
+                <PageTitle className='sign-in__page-title' title='Login' />
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
     }
 }
+
+SignIn = connect(null, actions)(SignIn);
 
 export default SignIn;
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 import SignUpForm from './signupForm';
 import PageTitle from '../pageTitle';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignUp extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
 
     onSubmit = (fields) => {
         console.log(fields);
@@ -12,11 +20,13 @@ class SignUp extends Component {
     render() {
         return (
             <div className='sign-up'>
-            <PageTitle className='sign-up__page-title' title='Register'/>
+                <PageTitle className='sign-up__page-title' title='Register' />
                 <SignUpForm onSubmit={this.onSubmit} className='sign-up__form' />
             </div>
         )
     }
 }
+
+SignUp = connect(null, actions)(SignUp);
 
 export default SignUp;

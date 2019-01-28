b252dd0d94f2be7656ace0520c3ecb1f00cc4427
macbookair <macbookair@macbookairs-MacBook-Air.local>
2019-01-28 14:50:08 -0800

    built long gray button component and placed instance of it on account information form to create change password button

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from '../formFields';
+import { FormInput, FormButton, LongGrayButton } from '../formFields';
 
 import history from '../../history';
 
 class AccountInformationForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+    
         return (
             <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
                 <Field className='account-information-form__name'
@@ -37,6 +38,7 @@ class AccountInformationForm extends Component {
                 name='city'
                 component={FormInput}/>
 
+
                 <Field className='account-information-form__state'
                 type='state'
                 title='State'
@@ -49,17 +51,16 @@ class AccountInformationForm extends Component {
                 placeholder='Zipcode'
                 name='zipcode'
                 component={FormInput}/>
-                
-                
 
-
-             
-                {/* <Field className='sign-in-form__password'
-                onClick={() => history.push('/account')}
-                type='password'
-                title='Password'
-                name='password'
-                component={FormButton}/> */}
+        
+                <Field className='account-information-form__change-password'
+                onClick={() => console.log('tryna show passwords')}
+                type='button'
+                labelTitle='Password'
+                title='Change Password'
+                name='change-password'
+                component={LongGrayButton}/>
+   
             </form>
         )
     }

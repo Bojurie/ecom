ac1cae466bc9c248cf261bb43194c2c10c2dc551
macbookair <macbookair@macbookairs-MacBook-Air.local>
2019-02-01 13:34:18 -0800

    develop shopreducer and fetch shop categories action creator

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -6,37 +6,35 @@ import * as actions from '../../actions';
 class Shop extends Component {
 
   componentDidMount() {
-    const headerLinks = [
-      {
-          _id: 0,
-          title: 'Login',
-          path: '/signin'
-      }
-  ]
-  this.props.setHeaderLinks(headerLinks);
-
-   
-    // fetch shop product ction creator
-    // fetch navbar links
-       // set navbar links
-      //  filter products with links
+      const headerLinks = [
+          {
+              _id: 0,
+              title: 'Login',
+              path: '/signin'
+          }
+      ]
+      this.props.setHeaderLinks(headerLinks);
+      this.props.fetchShopCategories();
+      // fetch navbar links
+          // set navbar links
+          // filter products with links
+      // fetch shop products action creator
   }
-
-  render(){
-    return(
-      <div className='shop'>
-         {/* search bar component */}
-         {/* shop product */}
-         {/* shop cart button */}
-      </div>
-    )
+  render() {
+      return (
+          <div className='shop'>
+              {/* shop search bar */}
+              {/* shop product */}
+              {/* shop cart button */}
+          </div>
+      )
   }
 }
 
-function mapStateToprops(state) {
-  return { state }
+function mapStateToProps(state) {
+  return { state } 
 }
 
-Shop = connect(mapStateToprops, actions)(Shop);
+Shop = connect(mapStateToProps, actions)(Shop);
 
 export default Shop;

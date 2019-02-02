974b44bd1fe5a9629a3f2f84b7920a0b7503578e
macbookair <macbookair@macbookairs-MacBook-Air.local>
2019-02-01 22:51:01 -0800

    filter product with query action creator

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,115 +1,116 @@
 import {
-  SET_SHOP_CATEGORIES,
-  SET_NAVBAR_LINKS,
-  SET_SHOP_PRODUCTS,
-  FILTER_PRODUCTS_WITH_CATEGORY_ID
+    SET_SHOP_CATEGORIES,
+    SET_NAVBAR_LINKS,
+    SET_SHOP_PRODUCTS,
+    FILTER_PRODUCTS_WITH_CATEGORY_ID,
+    FILTER_PRODUCTS_WITH_QUERY
 } from './types';
 
+export function filterProductsWithQuery(fields) {
+    return ({
+        type: FILTER_PRODUCTS_WITH_QUERY,
+        payload: fields
+    })
+}
+
 export function filterProductsWithCategoryId(_id) {
-  return ({
-      type: FILTER_PRODUCTS_WITH_CATEGORY_ID,
-      payload: _id
-  })
+    return ({
+        type: FILTER_PRODUCTS_WITH_CATEGORY_ID,
+        payload: _id
+    })
 }
 
 export function fetchShopCategories() {
-  return ({
-      type: SET_SHOP_CATEGORIES,
-      payload: [
-          {
-              _id: 0,
-              title: 'All'
-          },
-          {
-              _id: 1,
-              title: 'JavaScript'
-          },
-          {
-              _id: 2,
-              title: 'UI/UX'
-          },
-          {
-              _id: 3,
-              title: 'Linux'
-          },
-          {
-              _id: 4,
-              title: 'Python'
-          },
-          {
-              _id: 5,
-              title: 'UML'
-          },
-          {
-              _id: 6,
-              title: 'Ruby'
-          },
-      ]
+    return ({
+        type: SET_SHOP_CATEGORIES,
+        payload: [
+            {
+                _id: 0,
+                title: 'All'
+            },
+            {
+                _id: 1,
+                title: 'JavaScript'
+            },
+            {
+                _id: 2,
+                title: 'UI/UX'
+            },
+            {
+                _id: 3,
+                title: 'Linux'
+            },
+            {
+                _id: 4,
+                title: 'Python'
+            },
+            {
+                _id: 5,
+                title: 'UML'
+            },
+            {
+                _id: 6,
+                title: 'Ruby'
+            },
+        ]
 
-  })
+    })
 }
 
 
 export function fetchShopProducts() {
-  return ({
-      type: SET_SHOP_PRODUCTS,
-      payload: [
-          {
-              _id: 0,
-              title: 'JavaScript in the Browser',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 1.99,
-              belongsTo: [0, 1]
-          
-          },
-          {
-              _id: 1,
-              title: 'Graph Database',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 2.99,
-              belongsTo: [0, 6]
-             
-          },
-          {
-              _id: 2,
-              title: 'Full Stack Development',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 4.78,
-              belongsTo: [0, 1, 4]
-             
-          },
-          {
-              _id: 3,
-              title: 'JavaScript Development',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 3.49,
-              belongsTo: [0, 1]
-            
-          },
-          {
-              _id: 4,
-              title: 'User Interface Design',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 2.09,
-              belongsTo: [0, 2]
-          
-          },
-          {
-              _id: 5,
-              title: 'User Experience Design',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 3.99,
-              belongsTo: [0, 1]
-              
-          },
-          {
-              _id: 6,
-              title: 'Advanced OOP',
-              description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm',
-              price: 5.99,
-              belongsTo: [0, 6]
-           
-          },
-      ]
-  })
+    return ({
+        type: SET_SHOP_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                title: 'JavaScript in the Browser',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 1,
+                title: 'Graph Database',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 6]
+            },
+            {
+                _id: 2,
+                title: 'Full Stack Development',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 1, 4]
+            },
+            {
+                _id: 3,
+                title: 'User Interface Design',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 4,
+                title: 'JavaScript Development',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 5,
+                title: 'User Experience Design',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 6,
+                title: 'Advanced OOP',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
+                price: 1.99,
+                belongsTo: [0, 6]
+            },
+        ]
+    })
 }

ebb2ec2a643b482d37d1c0c266a4003e3fc51859
macbookair <macbookair@macbookairs-MacBook-Air.local>
2019-02-03 13:38:36 -0800

    structured shop cart component and styled

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
+
+  constructor() {
+      super()
+
+      this.state = {
+          showCart: true
+      }
+  }
 
   componentDidMount() {
       const headerLinks = [
@@ -23,30 +32,44 @@ class Shop extends Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    if(this.props != nextProps) {
-        this.props.setNavbarLinks(nextProps.categories,(_id) => this.props.filterProductsWithCategoryId(_id));
+      if(this.props != nextProps) {
+          this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
       }
       return true
   }
 
   onSubmit = (fields) => {
-    this.props.filterProductsWithQuery(fields);
+      this.props.filterProductsWithQuery(fields)
   }
 
+  handleAddToCart = () => {
+      if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+          document.getElementById('shop-cart').classList.remove('cart-hidden');
+      } else {
+          document.getElementById('shop-cart').classList.add('cart-hidden');
+      }
+  }
+
+
   render() {
+      // return <ShopCart className='shop__cart'/>
       return (
           <div className='shop'>
               <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
               <div className='shop__products'>
-                    {
-                        this.props.filteredProducts.map(product => {
-                            return (
-                                <ShopProduct {...product} key={product._id}/> 
-                            )
-                        })
-                    }
-                </div>
-              {/* shop cart button */}
+                  {
+                      this.props.filteredProducts.map(product => {
+                          return (
+                              <ShopProduct {...product} key={product._id} />
+                          )
+                      })
+                  }
+              </div>
+              {
+                  this.state.showCart ? <ShopCart className='shop__cart'/> : ''
+              }
+              
+              <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/>
           </div>
       )
   }
@@ -55,7 +78,7 @@ class Shop extends Component {
 function mapStateToProps(state) {
   const { categories, filteredProducts } = state.shop;
   return {
-      categories, 
+      categories,
       filteredProducts
   } 
 }

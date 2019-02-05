8f6a7a60efeb79ee011f3381c572993f6674d61f
macbookair <macbookair@macbookairs-MacBook-Air.local>
2019-02-05 09:45:18 -0800

    review form grid

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -4,36 +4,35 @@ import { reduxForm, Field } from 'redux-form';
 
 import { FormButton } from '../formFields';
 
-
 import history from '../../history';
 
 class ReviewForm extends Component {
-  render() {
-      const { className, handleSubmit } = this.props;
+    render() {
+        const { className, handleSubmit } = this.props;
 
-      return (
-          <form onSubmit={handleSubmit} className={`${className} review-form`}>
-              <div className='review-form__line'></div>
-              <Field className='review-form__proceed'
-              onClick={() => history.push('/account')}
-              type='submit'
-              title='Proceed to Checkout'
-              name='proceed'
-              component={FormButton}/>
-              <Field className='review-form__back'
-              onClick={() => history.push('/signin')}
-              type='button'
-              title='Back'
-              name='back'
-              short={true}
-              component={FormButton}/>
-          </form>
-      )
-  }
+        return (
+            <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                <div className='review-form__line'></div>
+                <Field className='review-form__proceed'
+                onClick={() => history.push('/signin')}
+                type='submit'
+                title='Proceed to Checkout'
+                name='proceed'
+                component={FormButton}/>
+                <Field className='review-form__back'
+                onClick={() => history.push('/shop')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true}
+                component={FormButton}/>
+            </form>
+        )
+    }
 }
 
 ReviewForm = reduxForm({
-  form: 'ReviewForm'
+    form: 'ReviewForm'
 })(ReviewForm);
 
 export default ReviewForm;

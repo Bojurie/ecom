c6fde96a13040af0c44c67641557b033f3cbb042
macbookair <macbookair@macbookairs-MacBook-Air.local>
2019-02-07 10:32:51 -0800

    layed out shipping form fields

--- src/components/information/shipping.js
+++ src/components/information/shipping.js
@@ -12,23 +12,23 @@ import ShippingForm from './shippingForm';
 
 class Shipping extends Component {
 
-    componentDidMount() {
-        this.props.setHeaderLinks([]);
-        this.props.setNavbarLinks([]);
-    }
+  componentDidMount() {
+      this.props.setHeaderLinks([]);
+      this.props.setNavbarLinks([]);
+  }
 
-    onSubmit = (fields) => {
-        console.log(fields);
-    }
+  onSubmit = (fields) => {
+      console.log(fields);
+  }
 
-    render() {
-        return (
-            <div className='shipping-form'>
-                <PageTitle className='shipping-form__page-title' title='Shipping Address' />
-                <ShippingForm onSubmit={this.onSubmit} className='shipping-form__form' />
-            </div>
-        )
-    }
+  render() {
+      return (
+          <div className='shipping'>
+              <PageTitle className='shipping__page-title' title='Shipping Address' />
+              <ShippingForm onSubmit={this.onSubmit} className='shipping__form' />
+          </div>
+      )
+  }
 }
 
 Shipping = connect(null, actions)(Shipping);
